6fb9dc1cee9adfcdc8437d6672235aa834746463
Dmitry <pchellovod@gmail.com>
2018-02-06 16:54:03 +0500

    add methods for login check

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Context
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ru
+
+    # Disable field_with_errors <div>
+    config.action_view.field_error_proc = Proc.new{ |html_tag, instance| "#{html_tag}".html_safe }
   end
 end

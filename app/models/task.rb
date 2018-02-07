4c450cccec5099435946d2010d0618ce365eaf46
Dmitry <pchellovod@gmail.com>
2018-02-07 14:02:26 +0500

    add context and recent scopes for tasks, fixed migration

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,15 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  validates :incontext, uniqueness: { scope: :user_id }, :if => Proc.new{ |task| task.incontext? }
+  validates :incontext, uniqueness: { scope: :user_id }, if: proc { |task| task.incontext? }
+
+  scope :recent, lambda.count do
+    tasks = limit(count).order('created at DESC')
+    add_length = count - task.size
+    tasks += Array.new(add_length) { Task.new }
+  end
 
   scope :incomplete, -> { where completed: false }
+  scope :context, -> { where incontext: true }
   scope :free, -> { where incontext: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202190724) do
+ActiveRecord::Schema.define(version: 20180206121949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20180202190724) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
+    t.integer "progress", default: 0
+    t.boolean "incontext", default: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
